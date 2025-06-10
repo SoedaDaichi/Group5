9fa8a3c8b5f35b715dfff3258f19ba26ff4d29f2
WINDOWS-C36ML9J\User <daichi_soeda@trustsystem.co.jp>
2025-06-10 09:57:55 +0900

    ログイン処理_1

--- Group5/src/main/java/services/auth.java
+++ Group5/src/main/java/services/auth.java
@@ -11,16 +11,16 @@ public class auth {
 
 	public static Users findByEmail(String mail) {
 		Users user = null;
-		String select = "SELECT * FROM users WHERE mail = ?";
+		String select = "SELECT * FROM accounts WHERE mail = ? AND authority = 11";
 		try (Connection conn = Db.open();
 				PreparedStatement ps = conn.prepareStatement(select)) {
 			ps.setString(1, mail);
 			ResultSet rs = ps.executeQuery();
 			if (rs.next()) {
 				user = new Users();
-				user.setMail(rs.getString("email"));
+				user.setMail(rs.getString("mail"));
 				user.setPass(rs.getString("password"));
-				user.setAccount_id(rs.getInt("id"));
+				user.setAccount_id(rs.getInt("account_id"));
 				user.setName(rs.getString("name"));
 			}
 		} catch (Exception e) {
@@ -41,17 +41,17 @@ public class auth {
 	}
 
 	public static boolean passCheck(int account_id, String cPass) {
-		String select = "SELECT password FROM users WHERE account_id = ?";
-		String password = null;
+		String select = "SELECT password FROM accounts WHERE account_id = ?";
+		String pass = null;
 
 		try (Connection conn = Db.open();
 				PreparedStatement ps = conn.prepareStatement(select)) {
 			ps.setInt(1, account_id);
 			ResultSet rs = ps.executeQuery();
 			if (rs.next()) {
-		        password = rs.getString("pass");
+		        pass = rs.getString("password");
 			}
-			if (!password.equals(cPass)) {
+			if (!pass.equals(cPass)) {
 				return false;
 			}
 		} catch (Exception e) {
