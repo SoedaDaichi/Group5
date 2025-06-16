c5a4ee3e98e2c068ddaf4c1d48d461407b648f8c
WINDOWS-C36ML9J\User <daichi_soeda@trustsystem.co.jp>
2025-06-16 15:03:46 +0900

    売上登録エラー処理＆入力要素session保存をオブジェクト単位に変更

---
+++ Group5/src/main/java/services/S0010Service.java
@@ -0,0 +1,36 @@
+package services;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class S0010Service {
+	public static boolean ValidDate(String dateStr) {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setLenient(false);
+		try {
+			sdf.parse(dateStr);
+			return false;
+		} catch (ParseException e) {
+			return true;
+		}
+	}
+
+	public static boolean InValidInt(String intStr) {
+		try {
+			Integer.parseInt(intStr);
+			return false;
+		} catch (NumberFormatException e) {
+			return true;
+		}
+	}
+
+	public static boolean ValidNull(String str) {
+		if (str == null || str.isEmpty()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
