e86bad367b68d25dda3c2faa8af94c2b79cd63bb
WINDOWS-SKPHD58\User <karin.maruyama@trustsystem.co.jp>
2025-06-20 10:21:55 +0900

    S0043修正

--- Group5/src/main/java/daos/S0043Dao.java
+++ Group5/src/main/java/daos/S0043Dao.java
@@ -1,22 +1,18 @@
 package daos;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
+import utils.Db;
+
 public class S0043Dao {
-	private static final String DB_URL = "jdbc:mariadb://localhost:3306/group5";
-	private static final String DB_USER = "root";
-	private static final String DB_PASS = "root";
 
 	public boolean update(int account_id, String name, String mail, String hashedPass, String authority) {
 		String sql = "UPDATE accounts SET name=?, mail=?, password=?, authority=? WHERE account_id=?";
 
-		try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+		try (
+			 Connection conn = Db.open();	
 			 PreparedStatement pstmt = conn.prepareStatement(sql)) {
-
-			// ※ account_id の取得はとりあえず
-			//int accountId = 1;
 
 			pstmt.setString(1, name);
 			pstmt.setString(2, mail);
@@ -33,24 +29,10 @@ public class S0043Dao {
 		}
 	}
 	
-//	public boolean delete(int accountId) {
-//	    String sql = "DELETE FROM accounts WHERE account_id = ?";
-//
-//	    try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
-//	         PreparedStatement stmt = conn.prepareStatement(sql)) {
-//
-//	        stmt.setInt(1, accountId);
-//	        int rowsAffected = stmt.executeUpdate();
-//	        return rowsAffected > 0;
-//
-//	    } catch (Exception e) {
-//	        e.printStackTrace();
-//	        return false;
-//	    }
-//	}
 	
 	public boolean delete(int accountId) {
-	    try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS)) {
+	    try (
+	        Connection conn = Db.open()){
 	        String sql = "DELETE FROM accounts WHERE account_id = ?";
 	        PreparedStatement stmt = conn.prepareStatement(sql);
 	        stmt.setInt(1, accountId);
