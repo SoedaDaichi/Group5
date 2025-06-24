187aacff1988120c81dd8781087c4545491296ca
Ryuki_miyata <ryuki_miyata@trustsystem.co.jp>
2025-06-24 16:58:10 +0900

    authority統一

--- Group5/src/main/java/beans/AccountsSearchForm.java
+++ Group5/src/main/java/beans/AccountsSearchForm.java
@@ -6,18 +6,15 @@ import lombok.Data;
 public class AccountsSearchForm {
 	private String name;
 	private String mail;
-	private int authority0;
-	private int authority1;
-	private int authority2;
-	private int authority3;
+	private String[] authority;
 
-	public AccountsSearchForm(String name, String mail, int authority0, int authority1, int authority2,
-			int authority3) {
+	public AccountsSearchForm(String name, String mail, String[] authority) {
 		this.name = name;
 		this.mail = mail;
-		this.authority0 = authority0;
-		this.authority1 = authority1;
-		this.authority2 = authority2;
-		this.authority3 = authority3;
+		this.authority = authority;
+	}
+
+	public String[] getAuthority() {
+		return authority;
 	}
 }
