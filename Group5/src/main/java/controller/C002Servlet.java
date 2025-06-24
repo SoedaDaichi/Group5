5fc06ed044e2dfa259e92b0434c06d61922faf0d
WINDOWS-C36ML9J\User <daichi_soeda@trustsystem.co.jp>
2025-06-24 15:10:48 +0900

    Reapply "a"
    This reverts commit 58306f468d1376599c842d58cbd98e835172afe1.

--- Group5/src/main/java/controller/C002Servlet.java
+++ Group5/src/main/java/controller/C002Servlet.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import java.util.Map;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -11,8 +12,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import daos.C002Dao;
 
 /**
- * Servlet implementation class C002Servlet
- */
+* Servlet implementation class C002Servlet
+*/
 @WebServlet("/C002.html")
 public class C002Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -32,20 +33,43 @@ public class C002Servlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		        C002Dao dao = new C002Dao();
-		        int kotoshi = java.time.Year.now().getValue();
-		        int month = java.time.LocalDate.now().getMonthValue();
+		C002Dao dao = new C002Dao();
+		int kotoshi = java.time.Year.now().getValue();
+		int month = java.time.LocalDate.now().getMonthValue();
 
-		        int monthSales = dao.getMonthSales(kotoshi, month);
-		        int nennkannSales = dao.getNennkannSales(kotoshi);
+		int monthSales = dao.getMonthSales(kotoshi, month);
+		int nennkannSales = dao.getNennkannSales(kotoshi);
+		Map<String, Integer> categorySalesMap = dao.getCategorySales();
+		//		        ObjectMapper mapper = new ObjectMapper();
+		//		        String categorySalesJson = mapper.writeValueAsString(categorySalesMap);
+		//		        
+		StringBuilder jsonBuilder = new StringBuilder();
+		jsonBuilder.append("{");
 
-		        request.setAttribute("monthSales", monthSales);
-		        request.setAttribute("nennkannSales", nennkannSales);
+		int count = 0;
+		for (Map.Entry<String, Integer> entry : categorySalesMap.entrySet()) {
+			jsonBuilder.append("\"")
+					.append(entry.getKey().replace("\"", "\\\""))
+					.append("\":")
+					.append(entry.getValue());
 
-		        request.getRequestDispatcher("/C002.jsp").forward(request, response);
-		    }
+			if (++count < categorySalesMap.size()) {
+				jsonBuilder.append(",");
+			}
+		}
 
-		
+		jsonBuilder.append("}");
+
+		String categorySalesJson = jsonBuilder.toString();
+
+		request.setAttribute("monthSales", monthSales);
+		request.setAttribute("nennkannSales", nennkannSales);
+		//  request.setAttribute("CategorySales", CategorySales);
+		request.setAttribute("categorySalesJson", categorySalesJson);
+
+		request.getRequestDispatcher("/C002.jsp").forward(request, response);
+	}
+
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
@@ -56,3 +80,14 @@ public class C002Servlet extends HttpServlet {
 	}
 
 }
+
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//
+//
+//C002Dao dao = new C002Dao();
+//Map<String, Integer> categorySalesMap = dao.getCategorySales();
+//
+//ObjectMapper mapper = new ObjectMapper();
+//String categorySalesJson = mapper.writeValueAsString(categorySalesMap);
+//
+//request.setAttribute("categorySalesJson", categorySalesJson);
